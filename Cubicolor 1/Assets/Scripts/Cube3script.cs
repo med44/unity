01f6588fdf90593df78df9e7fefe5c599d417beb
ARadovski <andrey@soundnoir.net>
2022-06-05 01:08:19 -0400

    Gitignore pruning the rest

---
+++ Cubicolor 1/Assets/Scripts/Cube3script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube3script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Cube1script.userPosX, 0, Cube1script.userPosZ * -1);  // Movement behaviour of the cube.
+    }
+}
